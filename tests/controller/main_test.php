e43d159977a7a96dc8071828164889e7079a169a
James J Myers <myersjj@gmail.com>
2018-01-07 16:32:19 -0800

    test

--- tests/controller/main_test.php
+++ tests/controller/main_test.php
@@ -10,7 +10,7 @@
  */
 namespace myersware\mapusers\tests\controller;
 
-require_once dirname(__FILE__) . '/../phpBB/includes/auth.php';
+require_once dirname(__FILE__) . '/../phpBB3/includes/auth.php';
 
 class main_test extends \phpbb_test_case {
 	public function handle_data() {

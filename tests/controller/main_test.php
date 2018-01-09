c3171777db24021af8215c1f6534d519493d3b59
James J Myers <myersjj@gmail.com>
2018-01-09 14:21:56 -0800

    test

--- tests/controller/main_test.php
+++ tests/controller/main_test.php
@@ -41,20 +41,6 @@ class main_test extends \phpbb_test_case {
 		$this->config = new \phpbb\config\config(array());
 		$phpbb_dispatcher = new \phpbb_mock_event_dispatcher();
 		$this->auth = $this->getMock('\phpbb\auth\auth');
-		/*
-		$this->auth = $this->getMockBuilder('\phpbb\auth\auth')
-		->disableOriginalConstructor()
-		->getMock();
-		$acl_get_map = array(
-				array('u_mapusers_view', 23, true),
-				array('u_mapusers_view', '23', true),// Called without int cast
-		);
-		$this->auth->expects($this->any())
-		->method('acl_get')
-		->with($this->stringContains('_'),
-				$this->anything())
-				->will($this->returnValueMap($acl_get_map));
-		*/
 		$this->template = $this->getMockBuilder('\phpbb\template\template')
 		->getMock();
 		$lang_loader = new \phpbb\language\language_file_loader($phpbb_root_path, $phpEx);
@@ -91,7 +77,7 @@ class main_test extends \phpbb_test_case {
 	
 	public function display_data() {
 		return array (
-				array('mapusers/showmap', 200, 'mapusers_body.html', 2)
+				array('mapusers/showmap', 403, 'mapusers_body.html', 2)
 		);
 	}
 	
@@ -108,56 +94,5 @@ class main_test extends \phpbb_test_case {
 		$this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $response);
 		$this->assertEquals($status_code, $response->getStatusCode());
 		$this->assertEquals($page_content, $response->getContent());
-	}
-	
-	public function xxhandle_data() {
-		return array (
-				array (
-						200,
-						'mapusers_body.html'
-				)
-		);
-	}
-	/**
-	 * @dataProvider handle_data
-	 */
-	public function xxtestxx_handle($status_code, $page_content) {
-		// Mocks are dummy implementations that provide the API of components we depend on //
-		/** @var \phpbb\template\template $template Mock the template class */
-		$template = $this->getMockBuilder('\phpbb\template\template')
-		->getMock();
-		/** @var \phpbb\user $user Mock the user class */
-		$user = $this->getMockBuilder ( '\phpbb\user' )->disableOriginalConstructor ()->getMock ();
-		
-		// Set user->lang() to return any arguments sent to it
-		$user->expects ( $this->any () )->method ( 'lang' )->will ( $this->returnArgument ( 0 ) );
-		
-		/** @var \phpbb\controller\helper $controller_helper Mock the controller helper class */
-		$controller_helper = $this->getMockBuilder ( '\phpbb\controller\helper' )->disableOriginalConstructor ()->getMock ();
-		
-		/** @var \phpbb\auth $auth Mock the auth class */
-		$auth = $this->getMock('\phpbb\auth\auth');
-		$acl_get_map = array(
-				array('u_mapusers_view', 23, true),
-				array('u_mapusers_view', '23', true),// Called without int cast
-		);
-		$auth->expects($this->any())
-		->method('acl_get')
-		->with($this->stringContains('_'),
-				$this->anything())
-				->will($this->returnValueMap($acl_get_map));
-		
-		// Set the expected output of the controller_helper->render() method
-		$controller_helper->expects ( $this->any () )->method ( 'render' )->willReturnCallback ( function ($template_file, $page_title = '', $status_code = 200, $display_online_list = false) {
-			return new \Symfony\Component\HttpFoundation\Response ( $template_file, $status_code );
-		} );
-		
-		// Instantiate the map users controller
-		$controller = new \myersware\mapusers\controller\main ( new \phpbb\config\config ( array () ), $controller_helper, $template, $user, $auth );
-		
-		$response = $controller->handle ( 'test' );
-		$this->assertInstanceOf ( '\Symfony\Component\HttpFoundation\Response', $response );
-		$this->assertEquals ( $status_code, $response->getStatusCode () );
-		$this->assertEquals ( $page_content, $response->getContent () );
 	}
 }

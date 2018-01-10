2e74b9e3a2c472994ddf70ea4c602d9fd3396624
James J Myers <myersjj@gmail.com>
2018-01-09 16:55:38 -0800

    test

--- tests/functional/demo_test.php
+++ tests/functional/demo_test.php
@@ -19,19 +19,44 @@ class demo_test extends \phpbb_functional_test_case {
 				'myersware/mapusers' 
 		);
 	}
+	
+	public function test_login()
+	{
+		$this->login();
+		// check for logout link
+		$crawler = self::request('GET', 'index.php');
+		$this->assertContains($this->lang('LOGOUT', 'admin'), $crawler->filter('.navbar')->text());
+	}
+	
+	public function test_login_other()
+	{
+		$this->create_user('anothertestuser');
+		$this->login('anothertestuser');
+		$crawler = self::request('GET', 'index.php');
+		$this->assertContains('anothertestuser', $crawler->filter('#username_logged_in')->text());
+	}
+	
 	public function test_myersware_mapusers() {
-		$crawler = self::request ( 'GET', 'app.php/mapusers/showmap' );
-		$this->assertContains ( 'myersware', $crawler->filter ( 'h2' )->text () );
+		$this->create_user('anothertestuser');
+		$this->login('anothertestuser');
+		try
+		{
+			$crawler = self::request ( 'GET', 'app.php/mapusers/showmap' );
+			$this->fail('The expected \phpbb\exception\http_exception was not thrown');
+		}
+		catch (\phpbb\exception\http_exception $exception)
+		{
+			$this->assertEquals(403, $exception->getStatusCode());
+			$this->assertEquals('NOT_AUTHORISED', $exception->getMessage());
+		}
 		
+		/*
+		$this->assertContains ( 'myersware', $crawler->filter ( 'h2' )->text () );
 		$this->add_lang_ext ( 'myersware/mapusers', 'common' );
 		$this->assertContains ( $this->lang ( 'MAPUSERS_HELLO', 'myersware' ), $crawler->filter ( 'h2' )->text () );
 		$this->assertNotContains ( $this->lang ( 'MAPUSERS_GOODBYE', 'myersware' ), $crawler->filter ( 'h2' )->text () );
 		
 		$this->assertNotContainsLang ( 'ACP_MAPUSERS', $crawler->filter ( 'h2' )->text () );
-	}
-	public function test_map_users() {
-		$crawler = self::request ( 'GET', 'app.php/mapusers/showmap' );
-		$this->assertNotContains ( 'myersware', $crawler->filter ( 'h2' )->text () );
-		$this->assertContains ( 'map', $crawler->filter ( 'h2' )->text () );
+		*/
 	}
 }

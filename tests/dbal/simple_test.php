89bdb18c761a56efb36cf90e1381bcde74e8ab96
James J Myers <myersjj@gmail.com>
2018-01-11 12:55:07 -0800

    tttt

--- tests/dbal/simple_test.php
+++ tests/dbal/simple_test.php
@@ -12,10 +12,33 @@ namespace myersware\mapusers\tests\dbal;
 
 class simple_test extends \phpbb_database_test_case {
 	static protected function setup_extensions() {
-		$this->assert(false, 'setup_extensions');
 		return array (
 				'myersware/mapusers' 
 		);
+	}
+	
+	public function setUp()
+	{
+		parent::setUp();
+		
+		global $table_prefix;
+		
+		$this->table_prefix = $table_prefix;
+		$this->db = $this->new_dbal();
+		$factory = new \phpbb\db\tools\factory ();
+		$this->db_tools = $factory->get ( $this->db );
+		$float_type = array (
+				'mysql_41' => 'decimal(11,8)',
+				'mysql_40' => 'decimal(11,8)',
+				'oracle' => 'number(11, 8)',
+				'sqlite3' => 'decimal(11, 8)'
+		);
+		foreach ( $float_type as $sql_layer => $type ) {
+			$this->db_tools->dbms_type_map [$sql_layer] ['FLOAT'] = $type;
+		}
+		var_dump($this->db_tools->dbms_type_map [$this->db->sql_layer]);
+		print "test FLOAT available";
+		flush();
 	}
 	
 	/** @var \phpbb\db\driver\driver_interface */
@@ -24,7 +47,6 @@ class simple_test extends \phpbb_database_test_case {
 		print "getDataSet";
 		echo "getDataSet";
 		flush();
-		$this->assertTrue(false, 'Asserting in getDataSet');
 		return $this->createXMLDataSet ( __DIR__ . '/fixtures/config.xml' );
 	}
 	
@@ -32,26 +54,7 @@ class simple_test extends \phpbb_database_test_case {
 		print "test_column";
 		echo "test_column";
 		flush();
-		$this->assertTrue(false, 'Asserting in test_column');
-		$this->db = $this->new_dbal ();
-		// This is how to instantiate db_tools in phpBB 3.2
-		$factory = new \phpbb\db\tools\factory ();
-		$db_tools = $factory->get ( $this->db );
-		
-		$float_type = array (
-				'mysql_41' => 'decimal(11,8)',
-				'mysql_40' => 'decimal(11,8)',
-				'oracle' => 'number(11, 8)',
-				'sqlite3' => 'decimal(11, 8)'
-		);
-		foreach ( $float_type as $sql_layer => $type ) {
-			$db_tools->dbms_type_map [$sql_layer] ['FLOAT'] = $type;
-		}
-		// var_dump($db_tools->dbms_type_map);
-		var_dump($db_tools->dbms_type_map [$sql_layer]);
-		print "test FLOAT available";
-		flush();
-		$this->assertArrayHasKey ( 'FLOAT', [$db_tools->dbms_type_map[$sql_layer] => 'no FLOAT type'] );
+		$this->assertArrayHasKey ( 'FLOAT', [$this->db_tools->dbms_type_map[$this->db->sql_layer] => 'no FLOAT type'] );
 		print "test table exists";
 		echo "test table exists";
 		flush();
@@ -59,7 +62,7 @@ class simple_test extends \phpbb_database_test_case {
 		print "test column exists";
 		echo "test column exists";
 		flush();
-		$this->assertTrue ( $db_tools->sql_column_exists ( $this->table_prefix . 'mapusers_geolocation', 'location' ), 'Asserting that column "location" exists' );
-		$this->assertFalse ( $db_tools->sql_column_exists ( $this->table_prefix . 'mapusers_geolocation', 'latitude_demo' ), 'Asserting that column "latitude_demo" does not exist' );
+		$this->assertTrue ( $this->db_tools->sql_column_exists ( $this->table_prefix . 'mapusers_geolocation', 'location' ), 'Asserting that column "location" exists' );
+		$this->assertFalse ( $this->db_tools->sql_column_exists ( $this->table_prefix . 'mapusers_geolocation', 'latitude_demo' ), 'Asserting that column "latitude_demo" does not exist' );
 	}
 }

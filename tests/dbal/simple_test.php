74aef5a4d0dee0d25f5ed4beae4aaa98c28bea71
James J Myers <myersjj@gmail.com>
2018-01-10 16:03:37 -0800

    ttt

--- tests/dbal/simple_test.php
+++ tests/dbal/simple_test.php
@@ -41,10 +41,16 @@ class simple_test extends \phpbb_database_test_case {
 		// var_dump($db_tools->dbms_type_map);
 		var_dump($db_tools->dbms_type_map [$sql_layer]);
 		print $db_tools->dbms_type_map [$sql_layer];
+		echo $db_tools->dbms_type_map [$sql_layer];
+		flush();
 		$this->assertArrayHasKey ( 'FLOAT', [$db_tools->dbms_type_map[$db_tools->sql_layer] => 'no FLOAT type'] );
 		print "test table exists";
+		echo "test table exists";
+		flush();
 		$this->assertTrue($this->db_tools->sql_table_exists($this->table_prefix . 'mapusers_geolocation'), 'Asserting that table "' . $this->table_prefix . 'geolocation" exists');
 		print "test column exists";
+		echo "test column exists";
+		flush();
 		$this->assertTrue ( $db_tools->sql_column_exists ( $this->table_prefix . 'mapusers_geolocation', 'location' ), 'Asserting that column "location" exists' );
 		$this->assertFalse ( $db_tools->sql_column_exists ( $this->table_prefix . 'mapusers_geolocation', 'latitude_demo' ), 'Asserting that column "latitude_demo" does not exist' );
 	}
